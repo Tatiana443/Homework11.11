6a88ee6f119aa5d0e2a9bd76e6b0768773133c15
Tatiana443 <tvb399@yandex.ru>
2022-11-16 17:43:39 +0600

    Project Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
