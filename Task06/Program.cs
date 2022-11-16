7804f68c2a8891093c0a91d11f26a59963836537
Tatiana443 <tvb399@yandex.ru>
2022-11-16 15:13:58 +0600

    Project Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+if (result == 0)
+    Console.WriteLine("ДА");
+else
+    Console.WriteLine("НЕТ");
