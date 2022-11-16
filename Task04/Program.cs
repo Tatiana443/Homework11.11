83a8151595bb2b878fed70a1358804b0594322b9
Tatiana443 <tvb399@yandex.ru>
2022-11-16 12:56:36 +0600

    Project Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+{
+    if (b > max) max = b;
+    if (c > max) max = c;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
