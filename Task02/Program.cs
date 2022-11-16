6d89079d3d9fa1f4323618af23fdcc235a42a181
Tatiana443 <tvb399@yandex.ru>
2022-11-16 11:36:04 +0600

    Project Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("Число a больше числа b");
+if (a < b)
+    Console.WriteLine("Число b больше числа a");
+if (a == b)
+    Console.WriteLine("Число a равно числу b");
